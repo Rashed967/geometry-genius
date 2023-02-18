8c87a50b8e46e6aa0ba322d3cf1d93a613d0dede
Rashed967 <rashedhasan967@gmail.com>
2023-02-18 13:06:06 +0600

    some msot important functin added

--- js/index.js
+++ js/index.js
@@ -1,13 +1,28 @@
+
+// first shap/Triangle script 
+
 document.getElementById('first-shap-btn').addEventListener('click', function(){
-    const rectangleShapText = document.getElementById('rectangle-shap').innerText;
-    const rectangleFormula = document.getElementById('rectangle-formula').innerText;
-    const li = document.createElement('li');
-    li.innerHTML = `
-    <div class="w-11/12 inline-flex justify-between">
-    <span>${rectangleShapText}</span> <span>12cm <sup>2</sup></span>
-<button class="text-white font-medium text-xs bg-blue-500 px-3 py-1  rounded-md">Covert to m <sup>2</sup> </button>
-</div>
-    `
-    const resultListContainer = document.getElementById('result-list-container');
-    resultListContainer.appendChild(li);
+    const textElementValue = getTextElementValueById('triangle-shape');
+    const firstTriangleValue = getInputElementValueById('triangle-first-input');
+    const secondTriangleValue = getInputElementValueById('triangle-second-input');
+
+    const total =  getMultiplyNumbers(firstTriangleValue, secondTriangleValue);
+
+    // set value in result 
+    setNewElementToResult(textElementValue, total);
 })
+
+
+
+// first shap/Triangle script 
+
+document.getElementById('first-shap-btn').addEventListener('click', function(){
+    const textElementValue = getTextElementValueById('triangle-shape');
+    const firstTriangleValue = getInputElementValueById('triangle-first-input');
+    const secondTriangleValue = getInputElementValueById('triangle-second-input');
+    const total =  getMultiplyNumbers(firstTriangleValue, secondTriangleValue);
+
+    // set value in result 
+    setNewElementToResult(textElementValue, total);
+})
+
--- js/utilities.js
+++ js/utilities.js
@@ -1,24 +1,45 @@
+// function for get text element value 
 function getTextElementValueById(TextElementId){
     const element = document.getElementById(TextElementId);
     const elementValue = element.innerText;
     return elementValue;
 }
 
+// function for get input field value 
 function getInputElementValueById(inputElementId){
     const element = document.getElementById(inputElementId);
     const elementValueString = element.value;
-    const elementValue = parseFloat(elementValueString);
-    return elementValue;
+    
+    // validate function 
+    if(typeof elementValueString === 'number' && elementValueString > 0){
+        const elementValue = parseFloat(elementValueString);
+        return elementValue;    
+    }
+    else{
+        alert('invalid numbera');
+        return;
+    }
+    
 }
 
-const firstTriangleValue = getInputElementValueById('triangle-first-input');
-const secondTriangleValue = getInputElementValueById('triangle-second-input');
-
-
+// function for multiply 2 input field value 
 function getMultiplyNumbers(value1, value2){
-    const totalOfMultiply = 0.5 * (value1 * value2); 
-    return totalOfMultiply;
+    let totalOfMultiply = 0.5 * (value1 * value2); 
+    return parseFloat(totalOfMultiply);
 }
 
-const total =  getMultiplyNumbers(firstTriangleValue, secondTriangleValue);
+
+// fucntion for set a new element/list to result area  
+function setNewElementToResult(value1, value2){
+    const li = document.createElement('li');
+    li.innerHTML = `
+    <div class="w-11/12 inline-flex justify-between">
+    <span>${value1}</span> <span>${value2} <sup>2</sup></span>
+<button class="text-white font-medium text-xs bg-blue-500 px-3 py-1  rounded-md">Covert to m <sup>2</sup> </button>
+</div>
+    `
+    const resultListContainer = document.getElementById('result-list-container');
+    resultListContainer.appendChild(li);
+}
+
 
